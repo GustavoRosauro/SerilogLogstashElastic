763da9742b7642e8633b6a8807832cb65ea48658
Gustavo Rosauro <gustavo.rosauro@cecred.coop.br>
2021-03-18 17:05:21 -0300

    'serilogexample'

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using Serilog;
+
+namespace SeriLogExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+            .WriteTo.Http("http://localhost:8080")
+            .CreateLogger();
+            for(int i = 0;i < 1000;i++)
+            {
+                string json = $"Numero: {i+1}, Indice: {i}";
+                log.Information("{"+json+"} registred");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
